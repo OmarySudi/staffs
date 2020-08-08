469b220ce488d1832e7d9d6fc54ae4cf62e640f6
sudi <kekovasudi@gmail.com>
2020-08-08 04:05:28 +0300

    editing welcome home view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<!-- <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
@@ -6,10 +6,10 @@
 
         <title>Laravel</title>
 
-        <!-- Fonts -->
+        Fonts 
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
-        <!-- Styles -->
+        Styles
         <style>
             html, body {
                 background-color: #fff;
@@ -97,4 +97,14 @@
             </div>
         </div>
     </body>
-</html>
+</html> -->
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+
+    <div class="row">
+            hEY THIS IS LIST OF STAFFS
+    </div>
+</div>
+@endsection
