f9131c0f71eeb46d2629ccf0a84a6682382012ce
sudiomary <kekovasudi@gmail.com>
2020-09-09 20:29:41 +0300

    adding width to tabs in staff blade

--- resources/views/staff.blade.php
+++ resources/views/staff.blade.php
@@ -5,7 +5,7 @@
 <div class="container" style="width: 70%">
     <div class="row justify-content-center">
         <div class="col-sm-12 col-md-9 mt-5">
-            <div class="row">
+            <!-- <div class="row"> -->
                 <div class="staff-left-column">
                  
                     <h3>{{ $staff->full_name }}</h3>
@@ -24,20 +24,20 @@
 
                      <div id="tabbed-content">
                         <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
-                            <li class="nav-item" role="presentation">
+                            <li class="nav-item" role="presentation" style="width: 30%">
                                 <a class="nav-link active" id="pills-about-tab" data-toggle="pill" href="#pills-about" role="tab" aria-controls="pills-about" aria-selected="true">About</a>
                             </li>
 
                             @foreach($roles as $role)
                                 @if($role->name == 'Lecturer')
-                                    <li class="nav-item" role="presentation">
+                                    <li class="nav-item" role="presentation" style="width: 30%">
                                         <a class="nav-link" id="pills-courses-tab" data-toggle="pill" href="#pills-courses" role="tab" aria-controls="pills-courses" aria-selected="false">Courses</a>
                                     </li>
                                     @break
                                 @endif
                             @endforeach
-                            <li class="nav-item" role="presentation">
-                                <a class="nav-link" id="pills-background-tab" data-toggle="pill" href="#pills-background" role="tab" aria-controls="pills-background" aria-selected="false">Background</a>
+                            <li class="nav-item" role="presentation" style="width: 30%">
+                                <a  class="nav-link" id="pills-background-tab" data-toggle="pill" href="#pills-background" role="tab" aria-controls="pills-background" aria-selected="false">Background</a>
                             </li>
                         </ul>
                         <div class="tab-content staff-dir" id="pills-tabContent">
@@ -84,15 +84,20 @@
                         </div>
                      </div>
                 </div>
-            </div>
+            <!-- </div> -->
         </div>
 
         <div class="col-sm-12 col-md-3 mt-5">
-            <div class="card mr-5 staff-card">
+            <div class="card-transparent mr-5 staff-card">
                 
                 <img class="card-img-top" style="height:120px" src="<?php echo asset("images/$staff->profile_picture_path")?>" alt="Card image cap">
-                <div class="card-body">
+                <div class="card-body" style="margin-top: 100px">
                     
+                    <p>{{ $staff->email}}</P>
+
+                    <p>{{ $staff->mobile_number}}</p>
+
+                    <p>{{ $staff->address }}</p>
                 </div>
             
         </div>
