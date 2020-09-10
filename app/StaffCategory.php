16651eb8c6285f4b6921c37141d1d5d580b852df
sudiomary <kekovasudi@gmail.com>
2020-09-10 15:25:02 +0300

    removing relationship from StaffCategory model

--- app/StaffCategory.php
+++ app/StaffCategory.php
@@ -7,13 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class StaffCategory extends Model
 {
     //
-
     protected $fillable = ['name'];
 
     protected $table = 'staff_categories';
-
-    public function staffs(){
-
-        return $this->hasMany('App\Staff');
-    }
 }
