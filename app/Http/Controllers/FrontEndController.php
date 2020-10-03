b958453a0cc2a552484b155b346f11dc751933e7
sudi <kekovasudi@gmail.com>
2020-10-03 20:00:55 +0300

    changing the displaying of staffs in frontEnd controller

--- app/Http/Controllers/FrontEndController.php
+++ app/Http/Controllers/FrontEndController.php
@@ -64,9 +64,9 @@ class FrontEndController extends Controller
                 //     '</a>'.
                 // '</div>';
 
-                '<div style="width: 12em;" class="mr-5 mb-4">'.
+                '<div style="width: 11em;" class="mr-1 mb-4">'.
                     '<div class="row">'.
-                        '<div class="card mr-4 mb-4 staff-card" style="width: 10rem;">'.
+                        '<div class="card mr-4 mb-4 staff-card" style="width: 9rem;">'.
                             '<a href="'.route( "staffs.staff-info",["id" => $staff->id]).'">'.
                                 '<img class="card-img-top" style="height:150px" src="images/'.$staff->profile_picture_path.'" alt="Card image cap">'.
                             '</a>'.
@@ -74,7 +74,7 @@ class FrontEndController extends Controller
                     '</div>'.
 
                     '<div class="row">'.
-                        '<div>'.
+                        '<div style="width: 9rem;">'.
                             '<h5 class="font-weight-bold">'.$staff->full_name.'</h5>'.
                             '<h6 class="margin-top:-1">'.$staff->job_title.'</h6>'.
                         '</div>'.
