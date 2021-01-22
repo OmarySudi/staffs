b1f2853a26bcc0718d4fe5950617378e70e97282
Sudi Omary <kekovasudi@gmail.com>
2021-01-22 09:13:18 +0300

    fixing login error in attempt method

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -51,6 +51,7 @@ class LoginController extends Controller
     protected function attemptLogin(Request $request)
     {
         return \Auth::attempt(
+            $this->credentials($request),
             $request->filled('remember')
         );
     }
